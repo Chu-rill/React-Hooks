26c1229dc69f457c28706d56d905dc796dcffe1e
Churchill <churchillrimamskep@gmail.com>
2024-04-03 20:13:44 +0100

    changed

--- src/DataFetching.jsx
+++ src/DataFetching.jsx
@@ -1,26 +1,29 @@
 import React, { useEffect, useState } from "react";
 
 function DataFetching() {
-  const [posts, setPosts] = useState([]);
+  const [post, setPost] = useState({});
+  const [id, setId] = useState(1);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
       .then((res) => res.json())
       .then((data) => {
-        setPosts(data);
+        setPost(data);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, []);
+  }, [id]);
 
   return (
     <>
-      <ul>
+      <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+      <div>{post.title}</div>
+      {/* <ul>
         {posts.map((post) => (
           <li key={post.id}>{post.title}</li>
         ))}
-      </ul>
+      </ul> */}
     </>
   );
 }
