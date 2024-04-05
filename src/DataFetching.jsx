3975ee8c3a010593acff6aa9acd9bb812d15b758
Churchill <churchillrimamskep@gmail.com>
2024-04-05 05:14:23 +0100

    changed

--- src/DataFetching.jsx
+++ src/DataFetching.jsx
@@ -3,6 +3,11 @@ import React, { useEffect, useState } from "react";
 function DataFetching() {
   const [post, setPost] = useState({});
   const [id, setId] = useState(1);
+  const [idFromButtonClick, setIdFromButtonClick] = useState(1);
+
+  const handleClick = () => {
+    setIdFromButtonClick(id);
+  };
 
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
@@ -13,11 +18,14 @@ function DataFetching() {
       .catch((err) => {
         console.log(err);
       });
-  }, [id]);
+  }, [idFromButtonClick]);
 
   return (
     <>
       <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+      <button type="button" onClick={handleClick}>
+        Fetch Post
+      </button>
       <div>{post.title}</div>
       {/* <ul>
         {posts.map((post) => (
@@ -31,4 +39,4 @@ function DataFetching() {
 export default DataFetching;
 
 //but i'll be using fetch
-//3.51
+//0.49
