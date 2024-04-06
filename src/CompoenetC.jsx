8694f4eac0fe0ac80e6ca2d29313c32cbad8715d
Churchill <churchillrimamskep@gmail.com>
2024-04-06 10:11:08 +0100

    changed

---
+++ src/CompoenetC.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ComponentE from "./ComponentE";
+function CompoenetC() {
+  return (
+    <>
+      <ComponentE />
+    </>
+  );
+}
+
+export default CompoenetC;
--- src/Context.jsx
+++ src/Context.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { createContext } from "react";
+import CompoenetC from "./CompoenetC";
 
+export const UserContext = createContext();
+export const ChannelContext = createContext();
 function Context() {
   return (
     <div>
-      <h1>hello world</h1>
+      <UserContext.Provider value={"churchill"}>
+        <ChannelContext value={"Codevolution"}>
+          <CompoenetC />
+        </ChannelContext>
+      </UserContext.Provider>
     </div>
   );
 }
 
 export default Context;
-//0.35
